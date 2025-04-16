a842c7ae300fb83349cf5fd9442275878f983b8b
RxNaison <87330825+RxNaison@users.noreply.github.com>
2025-04-16 20:46:20 +0700

    diagram button fix

--- app/src/main/java/com/rx/geminipro/ui/theme/Theme.kt
+++ app/src/main/java/com/rx/geminipro/ui/theme/Theme.kt
@@ -10,7 +10,7 @@ private val DarkColorScheme = darkColorScheme(
     primary = Blue,
     secondary = White,
     tertiary = White,
-    background = Black
+    background = Gray
 )
 
 private val LightColorScheme = lightColorScheme(
